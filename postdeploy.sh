6c334302d80e3043faa3acb1945effa96f6732da
Harry Dbrandy <devdbrandy@gmail.com>
2018-10-10 01:05:37 +0100

    Make use of heroku api platform for setting config var

--- postdeploy.sh
+++ postdeploy.sh
@@ -3,4 +3,9 @@
 set -o pipefail
 
 # Set config var APP_KEY with generated encryption key
-/usr/local/heroku/bin/heroku config:set APP_KEY=$(php artisan key:generate --show)
+curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_ID/config-vars \
+  -d '{
+  "APP_KEY": $(php artisan key:generate --show),
+}' \
+  -H "Accept: application/vnd.heroku+json; version=3" \
+  -H "Authorization: Bearer $HEROKU_API_KEY"
