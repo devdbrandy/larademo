0f4c0fdf4b29856256d2f23b1bba0f119f71fd70
Harry Dbrandy <devdbrandy@gmail.com>
2018-10-10 01:28:10 +0100

    Make provision for APP_KEY config var

--- postdeploy.sh
+++ postdeploy.sh
@@ -2,10 +2,4 @@
 
 set -o pipefail
 
-# Set config var APP_KEY with generated encryption key
-curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_ID/config-vars \
-  -d '{
-  "APP_KEY": $(php artisan key:generate --show),
-}' \
-  -H "Accept: application/vnd.heroku+json; version=3" \
-  -H "Authorization: Bearer $HEROKU_API_KEY"
+# Run custom commands
